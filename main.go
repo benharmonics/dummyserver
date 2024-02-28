643a018f774eed352df532b60b3c60f6929e722b
Benjamin Jiron <benjamin.jiron+git@gmail.com>
2024-02-27 22:13:27 -0700

    add option to log to file

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/benharmonics/dummyserver/router"
 )
@@ -13,15 +15,30 @@ var (
 	port         = flag.Int("port", 9090, "The port on which the server listens for incoming HTTP requests")
 	printHeaders = flag.Bool("headers", false, "If set, prints the headers for each received request")
 	ignoreParse  = flag.Bool("noparse", false, "If set, the server will not attempt to parse the request body")
+	logToFile    = flag.Bool("log", false, "If set, log to the log file")
+	logFile      = flag.String("logfile", "dummyserver.log", "Set the log file")
 )
 
 func main() {
 	flag.Parse()
 	router.PrintHeaders = *printHeaders
 	router.IgnoreParse = *ignoreParse
+	if *logToFile {
+		f, err := os.OpenFile(*logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer func(f *os.File) {
+			if err := f.Close(); err != nil {
+				log.Fatalln(err)
+			}
+		}(f)
+		wrt := io.MultiWriter(os.Stdout, f)
+		log.SetOutput(wrt)
+	}
 	s := http.NewServeMux()
 	s.HandleFunc("/", router.Router)
 	addr := fmt.Sprintf("localhost:%d", *port)
 	log.Println("[*] Listening on", addr)
-	panic(http.ListenAndServe(addr, s))
+	log.Fatalln(http.ListenAndServe(addr, s))
 }
