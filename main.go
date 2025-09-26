27894a707b038b6868d6417d5318dde5dd1df589
benharmonics <benjamin.jiron+git@gmail.com>
2025-09-25 23:57:54 -0600

    cors + allow other methods + allow ignoring parsing of body and/or headers

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 	"github.com/benharmonics/dummyserver/server"
 )
 
-var port = flag.Int("port", 9000, "The port on which the server listens for incoming HTTP requests")
-
 func main() {
+	port := flag.Int("port", 9000, "The port on which the server listens for incoming HTTP requests")
+	headers := flag.Bool("headers", false, "If true, prints the headers for each received request")
+	noparse := flag.Bool("noparse", false, "If true, the server will not attempt to parse the request body")
 	flag.Parse()
-	srv := server.NewServer()
+	srv := server.NewServer(*headers, *noparse)
 	addr := fmt.Sprintf("localhost:%d", *port)
 	log.Println("[*] Listening on", addr)
 	panic(http.ListenAndServe(addr, srv))
